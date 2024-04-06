816270635a7d0a7704bf52d0970e9b158681d58f
fleurette27 <gfleurette27@gmail.com>
2024-04-06 09:51:41 +0100

    Initial commit

---
+++ Shop/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from .models import Category, Product, Commande
+
+# Register your models here.
+
+admin.site.site_header = "E-commerce"
+admin.site.site_title = "fleurette shop"
+admin.site.index_title = "Manageur"
+
+
+class AdminCategorie(admin.ModelAdmin):
+    list_display = ('name', 'date_added')
+
+class AdminProduct(admin.ModelAdmin):
+    list_display = ('title', 'price', 'category', 'date_added')
+    search_fields = ('title',) 
+    list_editable = ('price',)
+
+class AdminCommande(admin.ModelAdmin):
+    list_display = ('items','nom','email','address', 'ville', 'pays','total', 'date_commande', )
+
+
+admin.site.register(Commande, AdminCommande)
+admin.site.register(Product, AdminProduct)
+admin.site.register(Category, AdminCategorie)
+
+
+
